955e019e91e25af1c0254ed64b25ca3311ba053f
fifi2018 <phyllis.gundu@qa.com>
2018-04-06 13:45:30 +0100

    Junit tests week 5 assesment

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/3e/7054bc23973900181931908707872cd8
@@ -0,0 +1,29 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import code.Mathematics;
+
+public class Mathematicstest {
+
+	@Test
+	public void addition() {
+		assertEquals(5,5);
+	}
+	
+	@Test
+	public void subtraction() {
+		assertEquals(50,60);
+	}
+
+	@Test
+	public void multiplication() {
+		assertEquals(20,20);
+	}
+	
+	@Test
+	public void division() {
+		assertEquals(4,4);
+	}
+}
---
+++ Java testing assesment/src/code/Mathematics.java
@@ -0,0 +1,41 @@
+package code;
+
+public class Mathematics {
+	
+	public static int addition(int a, int b)
+	{
+		a = 2;
+		b = 3;
+	
+     return(a+b);
+
+	}
+	
+	public static int subtractin(int a, int b)
+	{
+		a = 100;
+		b = 50;
+	
+     return(a-b);
+
+	}
+	
+	public static int multiplication(int a, int b)
+	{
+		a = 5;
+		b = 4;
+	
+     return(a*b);
+
+	}
+	
+	public static int division(int a, int b)
+	{
+		a = 40;
+		b = 10;
+	
+     return(a/b);
+
+	}
+	
+}
